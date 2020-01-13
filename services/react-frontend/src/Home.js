e7850492bc04ce0408c50ba0bd337f23e742e884
James <curley633@hotmail.com>
2020-01-13 20:39:07 +0000

    1 - Dockerfiles/docker-compose for react & express + basic home page

---
+++ services/react-frontend/src/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const Home = () => (
+        <div>
+            <h2>Home:</h2>
+            <p>Add to this later...</p>
+            <button onClick={displayMonroesEvents}>Monroes Events</button>
+        </div>
+)
+
+function displayMonroesEvents() {
+                
+}
