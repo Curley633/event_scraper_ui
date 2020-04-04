6121c55126d427a49d6e20bc258510a7d78f3e04
Curley633 <41597158+Curley633@users.noreply.github.com>
2020-04-04 11:39:59 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,4 @@
 
-sudo apt install -y rsync
-
 ERRORSTRING="Error. Please make sure you've indicated correct parameters"
 
 if [[ "$#" -eq 0 ]]
@@ -11,25 +9,20 @@ elif [ "$1" == "live" ]
         if [[ -z $2 ]]
             then
                 echo "Running dry-run"
-                rsync --dry-run -az --force --delete --progress --exclude-from=rsync_exclude.txt -e \
-                "ssh -i /C/vrworld-scoreboard-key.pem" \
+                rsync --dry-run -az --force --delete --progress --exclude-from=rsync_exclude.txt -e "ssh -p22" \
                 ~/project/* \
-                ubuntu@ec2-54-77-155-24.eu-west-1.compute.amazonaws.com:/home/ubuntu/usr/src/event_scraper_ui && echo "Dry Run Complete"
+                ubuntu@ec2-54-77-155-24.eu-west-1.compute.amazonaws.com:/home/ubuntu/usr/src/event_scraper_ui/ && echo "Dry Run Complete" || exit 1
 
         elif [[ "$2" == "go" ]]
             then
                 echo "Syncing Files"
-                rsync -az --force --delete --progress --exclude-from-rsync_exclude.txt -e \
-                #"ssh -i /C/vrworld-scoreboard-key.pem" \
+                rsync -az --force --delete --progress --exclude-from=rsync_exclude.txt -e "ssh -p22" \
                 ~/project/* \
-                ubuntu@ec2-54-77-155-24.eu-west-1.compute.amazonaws.com:/home/ubuntu/usr/src/event_scraper_ui/ echo "Sync Complete"
+                ubuntu@ec2-54-77-155-24.eu-west-1.compute.amazonaws.com:/home/ubuntu/usr/src/event_scraper_ui/ && echo "Sync Complete" || exit 1
         else
             echo $ERRORSTRING;
         fi
 fi
 
-time docker-compose -f docker-compose-prod.yml up -d || exit 1
+ssh ubuntu@ec2-54-77-155-24.eu-west-1.compute.amazonaws.com "cd usr/src/event_scraper_ui && time docker-compose -f docker-compose-prod.yml up -d && docker system prune --force -a && sudo service jenkins restart" || exit 1
 
-docker system prune --force -a
-
-#sudo service jenkins restart
